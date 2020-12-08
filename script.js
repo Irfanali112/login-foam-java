25a5efad00316052ab0fa6b704c43d5f777c2f1a
dfgfdg <you@example.com>
2020-12-08 17:56:47 +0500

    dfgfdg

--- script.js
+++ script.js
@@ -1,26 +1,88 @@
 
 
 
-var names=[];
-var emails=[];
-var passwaords=[];
+var names = [];
+var emails = [];
+var passwaords = [];
 
-function login(){
+function signup() {
 
-    names.push(document.getElementById("name").value);
+  names.push(document.getElementById("name").value);
 
-    emails.push(document.getElementById("email").value);
-    passwaords.push(document.getElementById("password").value);
- 
-      localStorage.setItem("names",JSON.stringify(name));
-      localStorage.setItem("emails",JSON.stringify(email));
-      localStorage.setItem("passwords",JSON.stringify(password));
+  emails.push(document.getElementById("email").value);
 
+  passwaords.push(document.getElementById("password").value);
 
-     window.location.href="login.html"
+  localStorage.setItem("name", JSON.stringify(names));
+  localStorage.setItem("email", JSON.stringify(emails));
+  localStorage.setItem("password", JSON.stringify(passwaords));
 
-      return false;
+  alert("signup success")
+  window.location.href = "login.html"
+
+  return false;
 
 }
 
 
+
+names = JSON.parse(localStorage.getItem("name"));
+emails = JSON.parse(localStorage.getItem("email"));
+passwaords = JSON.parse(localStorage.getItem("password"));
+
+function login() {
+
+
+  const email = document.getElementById("email1").value;
+  const passwords = document.getElementById("pass1").value;
+
+  
+
+  let isfound = false;
+
+  for (i = 0; i < email.length; i++)
+
+    if (email === emails[i]) {
+
+      isfound = i;
+      localStorage.setItem("isfound", JSON.stringify(isfound));
+      break;
+
+    }
+
+  if (isfound === false) {
+
+    alert("user is not found")
+  }
+  else if (passwaords[isfound] === passwords) {
+
+    alert("login success");
+    window.location.href = "./detail.html"
+
+  } else {
+
+    alert("user name or password wrong")
+
+
+  }
+
+  return false;
+
+}
+
+var index = JSON.parse(localStorage.getItem("isfound"));
+
+
+
+function loginsucess() {
+
+  document.getElementById("email").innerHTML = names[index];
+  document.getElementById("name").innerHTML = emails[index];
+
+
+
+
+
+
+
+}
